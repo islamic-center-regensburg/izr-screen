5f408d2fbaef2ef66731fdced4d33558257f3fd3
Mohamed Amine Bellil <mohamed.amine.bellil@outlook.de>
2026-02-23 17:16:10 +0100

    feat: add TickingHour component and integrate it into the Footer

---
+++ src/pages/prayer-times-page/TickingHour.tsx
@@ -0,0 +1,28 @@
+import { useEffect, useState } from "react";
+
+function pad(num: number): string {
+	return num.toString().padStart(2, "0");
+}
+
+function TickingHour() {
+	const [now, setNow] = useState(new Date());
+
+	useEffect(() => {
+		const interval = setInterval(() => {
+			setNow(new Date());
+		}, 1000);
+		return () => clearInterval(interval);
+	}, []);
+
+	const hours = pad(now.getHours());
+	const minutes = pad(now.getMinutes());
+	const seconds = pad(now.getSeconds());
+
+	return (
+		<span className="absolute right-0 mx-5 bg-primary text-white font-black p-2 rounded-2xl min-w-28 text-4xl">
+			{hours}:{minutes}:{seconds}
+		</span>
+	);
+}
+
+export default TickingHour;
--- src/pages/prayer-times-page/footer.tsx
+++ src/pages/prayer-times-page/footer.tsx
@@ -1,14 +1,11 @@
-import { Crosshair, Globe, MapPin, Phone } from "lucide-react";
+import { MapPin, Phone } from "lucide-react";
+import TickingHour from "./TickingHour";
 
 interface FooterProps {
 	address?: string;
-	city?: string;
-	country?: string;
-	latitude?: number;
-	longitude?: number;
 }
 
-function Footer({ address, city, country, latitude, longitude }: FooterProps) {
+function Footer({ address }: FooterProps) {
 	return (
 		<footer className="mt-[1.5vh] pt-[1vh] text-center text-[1.2vw] text-muted-foreground">
 			<div className="flex flex-wrap items-center justify-center gap-[1vw]">
@@ -18,24 +15,10 @@ function Footer({ address, city, country, latitude, longitude }: FooterProps) {
 				</div>
 				<span>•</span>
 				<div className="flex items-center gap-[0.5vw]">
-					<Globe size="1.2vw" />
-					<span>
-						{city}, {country}
-					</span>
+					<span>IBAN: DE30 7505 0000 0026 7651 56</span>
 				</div>
 				<span>•</span>
 				<div className="flex items-center gap-[0.5vw]">
-					<Crosshair size="1.2vw" />
-					<span>Lat: {latitude?.toFixed(4)}</span>
-				</div>
-				<span>•</span>
-				<div className="flex items-center gap-[0.5vw]">
-					<Crosshair size="1.2vw" />
-					<span>Lon: {longitude?.toFixed(4)}</span>
-				</div>
-				<span>•</span>
-				<div className="flex items-center gap-[0.5vw]">
-					<Globe size="1.2vw" />
 					<span>iz-regensburg.de</span>
 				</div>
 				<span>•</span>
@@ -43,6 +26,7 @@ function Footer({ address, city, country, latitude, longitude }: FooterProps) {
 					<Phone size="1.2vw" />
 					<span>017660800940</span>
 				</div>
+				<TickingHour />
 			</div>
 		</footer>
 	);
